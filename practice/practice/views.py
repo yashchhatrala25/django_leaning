333d6a86b6772d1bc09fcdbd9f963a50c462b61a
yashchhatrala25 <yashchhatrala25@gmail.com>
2025-05-01 23:28:45 +0530

    add demo template

--- practice/practice/views.py
+++ practice/practice/views.py
@@ -2,20 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def homePage(request):
-    data={
-        'title': 'Home',
-        'bdata': 'Welcome to Home Page | Dynamic Data',
-        'clist': ['PHP', 'Java', 'Python'],
-        'numbers': [],
-        'student_details': [
-            {'name': 'yash', 'phone': 123456789},
-            {'name': 'john', 'phone': 896764464},
-        ]
-    }
-    return render(request, "index.html", data)
+    # data={
+    #     'title': 'Home',
+    #     'bdata': 'Welcome to Home Page | Dynamic Data',
+    #     'clist': ['PHP', 'Java', 'Python'],
+    #     'numbers': [],
+    #     'student_details': [
+    #         {'name': 'yash', 'phone': 123456789},
+    #         {'name': 'john', 'phone': 896764464},
+    #     ]
+    # }
+    return render(request, "index.html")
 
 def aboutUs(request):
-    return HttpResponse("<h1>Welcome to About Page</h1>")
+    return render(request, 'about.html')
 
 def course(request):
     return HttpResponse("<h1>Welcome to Course Page</h1>")
